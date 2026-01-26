4cc0218f9ef9a8e2bd2672edcc66a248041c1636
x <null>
2026-01-26 15:03:16 +0700

    feat: enhance SELECT component with states management and selected attribute handling

--- src/UI/components/select/index.js
+++ src/UI/components/select/index.js
@@ -1,15 +1,19 @@
+import States from "/core/States.js"
 import { render } from "/core/UI.js"
 import { template } from "./template.js"
 
 export class SELECT extends HTMLElement {
     constructor() {
         super()
+        this.states = new States({ options: [], selected: null })
         this.attachShadow({ mode: "open" })
         render(template, this.shadowRoot)
+        this.subscriptions = []
+        this.render = this.render.bind(this)
     }
 
     static get observedAttributes() {
-        return ["data-name", "data-placeholder", "data-required"]
+        return ["data-name", "data-placeholder", "data-required", "data-selected"]
     }
 
     attributeChangedCallback(name, last, value) {
@@ -20,6 +24,10 @@ export class SELECT extends HTMLElement {
             if (value !== null) this.select.setAttribute("required", "")
             else this.select.removeAttribute("required")
         }
+        if (name === "data-selected") {
+            this.states.set({ selected: value })
+            this.select.value = value
+        }
     }
 
     connectedCallback() {
@@ -28,6 +36,21 @@ export class SELECT extends HTMLElement {
         if (this.dataset.required) this.select.setAttribute("required", "")
         if (this.dataset.name) this.select.setAttribute("name", this.dataset.name)
         if (this.dataset.placeholder) this.placeholder.dataset.key = this.dataset.placeholder
+        this.subscriptions.push(this.states.on("options", this.render))
+    }
+
+    disconnectedCallback() {
+        this.subscriptions.forEach((off) => off())
+    }
+
+    render() {
+        this.select.querySelectorAll("option:not([disabled])").forEach(option => option.remove())
+        const options = this.states.get("options").map(option => html`
+            <option value="${option.value}" ${option.value === this.states.get("selected") ? "selected" : ""}>
+                ${option.label}
+            </option>
+        `)
+        render(options, this.select, { append: true })
     }
 }
 
