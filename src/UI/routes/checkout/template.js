8a62c3d870175758fba09d3ebdb5c815edc0eea8
x <null>
2026-02-18 13:01:22 +0700

    feat: add checkout route with template and component structure
    - Created a new checkout route in `src/UI/routes/checkout/index.js` that defines a custom element `route-checkout`.
    - Implemented a shadow DOM for the checkout component and rendered its template.
    - Added a corresponding template file in `src/UI/routes/checkout/template.js` that includes the main layout and context components.

---
+++ src/UI/routes/checkout/template.js
@@ -0,0 +1,14 @@
+import "/UI/layouts/main/index.js"
+import "/UI/components/context/index.js"
+import { html } from "/core/UI.js"
+import "/UI/components/addresses/index.js"
+
+export const template = html`
+    <layout-main>
+        <h1><ui-context data-key="dictionary.checkout" /></h1>
+        <div>
+            
+        </div>
+    </layout-main>
+`
+export default template
